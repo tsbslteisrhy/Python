25004091bad23cd44b04c6e25439df605c79a9f6
tsbslteisrhy <tsbslteisrhy@naver.com>
2020-06-23 12:29:42 +0900

    2020/06/22 2-5 교재 예제 추가

--- Ch02/2-5.py
+++ Ch02/2-5.py
@@ -45,9 +45,32 @@ print('dic1 :', dic1)
 
 #Key 리스트 만들기(keys)
 a = {'name': 'pey', 'phone': '0119993323', 'birth': '1118'}
+print(a.keys())
+
+for k in a.keys():
+    print(k)
+
+print(list(a.keys()))
 
 #Value 리스트 만들기(values)
+print(a.values())
+
 #Key, Value 쌍 얻기(items)
+print(a.items())
+
 #Key: Value 쌍 모두 지우기(clear)
+a.clear()
+print(a)
+
 #Key로 Value 얻기(get)
+a = {'name': 'pey', 'phone': '0119993323', 'birth': '1118'}
+print(a.get('name'))
+print(a.get('phone'))
+print(a.get('nokey'))   #None 리턴
+#print(a['nokey'])      #error
+print(a.get('foo', 'bar'))
+
 #해당 Key가 딕셔너리 안에 있는지 조사하기(in)
+a = {'name': 'pey', 'phone': '0119993323', 'birth': '1118'}
+print('name' in a)
+print('email' in a)
