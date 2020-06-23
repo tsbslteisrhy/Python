4a1f326bc57c669c215339f06a4aa6ab6d261e49
tsbslteisrhy <tsbslteisrhy@naver.com>
2020-06-23 09:13:09 +0900

    2020/06/22

---
+++ Ch01/hello.py
@@ -0,0 +1,12 @@
+"""
+    날짜 : 2020/06/22
+    이름 : 유효진
+    내용 : Python HelloWorld 출력하기
+"""
+
+print('Hello World')
+print("Hello Python!")
+
+# 한줄로 출력하기
+print('Hello', end=', ')
+print('Mr.Kim!')
